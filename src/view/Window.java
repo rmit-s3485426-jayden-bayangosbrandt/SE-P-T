9ba9bd60d8ed1f6fdc2e9258cd918c2d4287e5f2
Jayden Bayangos-Brandt <s3485426@student.rmit.edu.au>
2016-03-21 14:44:35 +1100

    re-organising window

---
+++ src/view/Window.java
@@ -0,0 +1,22 @@
+package view;
+
+import javax.swing.*;
+import java.awt.*;
+
+/**
+ * Created by Jayden on 21/03/2016.
+ */
+public class Window extends JFrame {
+
+    public Window() throws HeadlessException {
+        this.setLayout(new BoxLayout(this, BoxLayout.Y_AXIS));
+        JPanel mainPanel = new JPanel();
+        mainPanel.setLayout(new GridLayout(1, 3));
+
+        this.setContentPane(mainPanel);
+        mainPanel.add(new JLabel("Test"));
+        this.setSize(500, 500);
+        this.setResizable(false);
+        this.setVisible(true);
+    }
+}
--- src/view/main.java
+++ src/view/main.java
@@ -9,17 +9,6 @@ import java.awt.*;
 public class main {
 
     public static void main(String[] args) {
-        JFrame frame = new JFrame();
-        frame.setLayout(new BoxLayout(frame, BoxLayout.Y_AXIS));
-        JPanel mainPanel = new JPanel();
-        mainPanel.setLayout(new GridLayout(1, 3));
-
-        frame.setContentPane(mainPanel);
-//        frame.add(new J)
-        mainPanel.add(new JLabel("Test"));
-        frame.setSize(500, 500);
-        frame.setResizable(false);
-//        frame.pack();
-        frame.setVisible(true);
+        JFrame frame = new Window();
     }
 }
