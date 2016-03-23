eab08eba23c47402ca355f83fe91df1e916708f1
Jayden Bayangos-Brandt <s3485426@student.rmit.edu.au>
2016-03-23 22:46:19 +1100

    implemented favourites ui and user name partially

--- src/view/Window.java
+++ src/view/Window.java
@@ -6,15 +6,58 @@ import java.awt.*;
 
 public class Window extends JFrame {
 
+    private JLabel userName = new JLabel("Name");
+    private Object[] objects = new Object[]{"1", "2", "3"};
+
     public Window() throws HeadlessException {
-        this.setLayout(new BoxLayout(this, BoxLayout.Y_AXIS));
+        this.setLayout(new BorderLayout());
         JPanel mainPanel = new JPanel();
-        mainPanel.setLayout(new GridLayout(1, 3));
+        mainPanel.setLayout(new BoxLayout(mainPanel, BoxLayout.Y_AXIS));
 
         this.setContentPane(mainPanel);
-        mainPanel.add(new JLabel("Test"));
+        mainPanel.add(getNorthPanel(), BorderLayout.NORTH);
         this.setSize(500, 500);
         this.setResizable(false);
         this.setVisible(true);
+    }
+
+    private JPanel getNorthPanel(){
+        JPanel north = new JPanel();
+        north.setPreferredSize(new Dimension(500, 100));
+        north.setLayout(new BoxLayout(north, BoxLayout.X_AXIS));
+        JPanel namePanel = new JPanel();
+        JPanel favePanel = new JPanel();
+
+        namePanel.setLayout(new BoxLayout(namePanel, BoxLayout.Y_AXIS));
+        namePanel.add(userName, 0);
+//        namePanel.add(Box.createRigidArea(new Dimension(300,0)), 1);
+
+        favePanel.setLayout(new BoxLayout(favePanel, BoxLayout.Y_AXIS));
+        // Setup favourite list component
+        JList list = new JList(objects);
+        list.setSelectionMode(ListSelectionModel.SINGLE_INTERVAL_SELECTION);
+        list.setLayoutOrientation(JList.VERTICAL);
+        list.setVisibleRowCount(3);
+        JScrollPane scrollPane = new JScrollPane(list);
+        scrollPane.setPreferredSize(new Dimension(100, 20));
+        // Add component to panel
+        favePanel.add(new JLabel("Favourites"));
+        favePanel.add(scrollPane, 1);
+
+        north.add(namePanel, 0);
+        north.add(Box.createRigidArea(new Dimension(300,0)), 1);
+        north.add(favePanel, 2);
+
+
+
+        return north;
+    }
+
+    public JLabel getUserName() {
+        return userName;
+    }
+
+    public void setUserName(JLabel userName) {
+        this.userName = userName;
     }
 }
