c4d369d4da63c2779525f8cc69f520daa32b31a5
Jayden Bayangos-Brandt <s3485426@student.rmit.edu.au>
2016-03-24 10:05:29 +1100

    added more components gui

--- src/view/Window.java
+++ src/view/Window.java
@@ -7,6 +7,7 @@ import java.awt.*;
 public class Window extends JFrame {
 
     private JLabel userName = new JLabel("Name");
+    private JTextField search = new JTextField("Search weather stations");
     private Object[] objects = new Object[]{"1", "2", "3"};
 
     public Window() throws HeadlessException {
@@ -16,14 +17,15 @@ public class Window extends JFrame {
 
         this.setContentPane(mainPanel);
         mainPanel.add(getNorthPanel(), BorderLayout.NORTH);
-        this.setSize(500, 500);
+        mainPanel.add(getCenterPanel(), BorderLayout.CENTER);
+        this.setSize(new Dimension(500, 500));
         this.setResizable(false);
         this.setVisible(true);
     }
 
     private JPanel getNorthPanel(){
         JPanel north = new JPanel();
-        north.setPreferredSize(new Dimension(500, 100));
+//        north.setSize(new Dimension(50, 50));
         north.setLayout(new BoxLayout(north, BoxLayout.X_AXIS));
         JPanel namePanel = new JPanel();
         JPanel favePanel = new JPanel();
@@ -33,13 +35,15 @@ public class Window extends JFrame {
 //        namePanel.add(Box.createRigidArea(new Dimension(300,0)), 1);
 
         favePanel.setLayout(new BoxLayout(favePanel, BoxLayout.Y_AXIS));
+//        favePanel.setPreferredSize(new Dimension(50, 20));
         // Setup favourite list component
         JList list = new JList(objects);
         list.setSelectionMode(ListSelectionModel.SINGLE_INTERVAL_SELECTION);
         list.setLayoutOrientation(JList.VERTICAL);
+//        list.setPreferredSize(new Dimension(50, 20));
         list.setVisibleRowCount(3);
         JScrollPane scrollPane = new JScrollPane(list);
-        scrollPane.setPreferredSize(new Dimension(100, 20));
+//        scrollPane.setPreferredSize(new Dimension(50, 20));
         // Add component to panel
         favePanel.add(new JLabel("Favourites"));
         favePanel.add(scrollPane, 1);
@@ -53,11 +57,43 @@ public class Window extends JFrame {
         return north;
     }
 
+    private JPanel getCenterPanel(){
+        JPanel center = new JPanel();
+        center.setLayout(new BoxLayout(center, BoxLayout.X_AXIS));
+        JPanel stationSelection = new JPanel();
+        JPanel stationSearch = new JPanel();
+        JComboBox area = new JComboBox(objects);
+        JComboBox region = new JComboBox(objects);
+        JComboBox station = new JComboBox(objects);
+        stationSelection.setLayout(new BoxLayout(stationSelection, BoxLayout.Y_AXIS));
+        stationSelection.add(new JLabel("Please select a weather station"), 0);
+        stationSelection.add(area, 1);
+        stationSelection.add(region, 2);
+        stationSelection.add(station, 3);
+        stationSearch.setLayout(new BoxLayout(stationSearch, BoxLayout.Y_AXIS));
+        stationSearch.add(new JLabel("Or search for a specific station"), 0);
+        stationSearch.add(search, 1);
+        stationSearch.add(Box.createVerticalStrut(100));
+        center.add(stationSelection, 0);
+        center.add(Box.createHorizontalStrut(100), 1);
+        center.add(stationSearch, 2);
+
+        return center;
+    }
+
     public JLabel getUserName() {
         return userName;
     }
 
     public void setUserName(JLabel userName) {
         this.userName = userName;
+    }
+
+    public JTextField getSearch() {
+        return search;
+    }
+
+    public void setSearch(JTextField search) {
+        this.search = search;
     }
 }
