4ba042fcb1f04f0bca88d398a89392a66fb0d424
Jayden Bayangos-Brandt <s3485426@student.rmit.edu.au>
2016-03-24 12:00:27 +1100

    partially adding connections to model from MainWindow

--- src/model/Model.java
+++ src/model/Model.java
@@ -1,13 +1,27 @@
 package model;
 
 
+import com.sun.javafx.stage.WindowHelper;
+import view.MainWindow;
+
 public class Model {
     private static Model ourInstance = new Model();
+
+    private MainWindow mainWindow;
 
     public static Model getInstance() {
         return ourInstance;
     }
 
     private Model() {
+    }
+
+    public void setMainWindow(MainWindow window){
+        this.mainWindow = window;
+    }
+
+    public void changeDataset(){
+        String[] strings = new String[]{"a", "b", "c"};
+        mainWindow.setAreaDataset(strings);
     }
 }
