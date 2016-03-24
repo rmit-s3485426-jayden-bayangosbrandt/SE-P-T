2ba5b7b3a13bfea38dd0d4a4f92729a9482f2244
Jayden Bayangos-Brandt <s3485426@student.rmit.edu.au>
2016-03-24 11:59:59 +1100

    rename window to MainWindow

--- src/view/MainWindow.java
+++ src/view/MainWindow.java
@@ -4,13 +4,15 @@ import javax.swing.*;
 import java.awt.*;
 
 
-public class Window extends JFrame {
+public class MainWindow extends JFrame {
 
     private JLabel userName = new JLabel("Name");
     private JTextField search = new JTextField("Search weather stations");
-    private Object[] objects = new Object[]{"1", "2", "3"};
+    private String[] areaDataset = new String[]{"1", "2", "3"};
+    private String[] regionDataset = new String[]{"1", "2", "3"};
+    private String[] stationDataset = new String[]{"1", "2", "3"};
 
-    public Window() throws HeadlessException {
+    public MainWindow() throws HeadlessException {
         this.setLayout(new BorderLayout());
         JPanel mainPanel = new JPanel();
         mainPanel.setLayout(new BoxLayout(mainPanel, BoxLayout.Y_AXIS));
@@ -30,14 +32,16 @@ public class Window extends JFrame {
         JPanel namePanel = new JPanel();
         JPanel favePanel = new JPanel();
 
-        namePanel.setLayout(new BoxLayout(namePanel, BoxLayout.Y_AXIS));
-        namePanel.add(userName, 0);
+        namePanel.setLayout(new BoxLayout(namePanel, BoxLayout.X_AXIS));
+        namePanel.add(Box.createHorizontalGlue());
+        namePanel.add(userName);
+        namePanel.add(Box.createHorizontalGlue());
 //        namePanel.add(Box.createRigidArea(new Dimension(300,0)), 1);
 
         favePanel.setLayout(new BoxLayout(favePanel, BoxLayout.Y_AXIS));
 //        favePanel.setPreferredSize(new Dimension(50, 20));
         // Setup favourite list component
-        JList list = new JList(objects);
+        JList list = new JList(areaDataset);
         list.setSelectionMode(ListSelectionModel.SINGLE_INTERVAL_SELECTION);
         list.setLayoutOrientation(JList.VERTICAL);
 //        list.setPreferredSize(new Dimension(50, 20));
@@ -49,7 +53,7 @@ public class Window extends JFrame {
         favePanel.add(scrollPane, 1);
 
         north.add(namePanel, 0);
-        north.add(Box.createRigidArea(new Dimension(300,0)), 1);
+        north.add(Box.createHorizontalGlue(), 1);
         north.add(favePanel, 2);
 
 
@@ -62,9 +66,9 @@ public class Window extends JFrame {
         center.setLayout(new BoxLayout(center, BoxLayout.X_AXIS));
         JPanel stationSelection = new JPanel();
         JPanel stationSearch = new JPanel();
-        JComboBox area = new JComboBox(objects);
-        JComboBox region = new JComboBox(objects);
-        JComboBox station = new JComboBox(objects);
+        JComboBox area = new JComboBox(areaDataset);
+        JComboBox region = new JComboBox(areaDataset);
+        JComboBox station = new JComboBox(areaDataset);
         stationSelection.setLayout(new BoxLayout(stationSelection, BoxLayout.Y_AXIS));
         stationSelection.add(new JLabel("Please select a weather station"), 0);
         stationSelection.add(area, 1);
@@ -95,5 +99,17 @@ public class Window extends JFrame {
 
     public void setSearch(JTextField search) {
         this.search = search;
+    }
+
+    public void setAreaDataset(String[] strings){
+        this.areaDataset = strings;
+    }
+
+    public void setRegionDataset(String[] strings){
+        this.regionDataset = strings;
+    }
+
+    public void setStationDataset(String[] strings){
+        this.stationDataset = strings;
     }
 }
