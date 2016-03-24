69156cf13abe5f1e73b84571678314260ff7bd63
Jayden Bayangos-Brandt <s3485426@student.rmit.edu.au>
2016-03-24 12:00:51 +1100

    some changes

--- src/view/main.java
+++ src/view/main.java
@@ -1,13 +1,17 @@
 package view;
 
+import model.Model;
+import sun.applet.Main;
+
 import javax.swing.*;
-import java.awt.*;
 
 
 public class main {
 
+    private Model model = Model.getInstance();
+
     public static void main(String[] args) {
-        JFrame frame = new Window();
+        JFrame frame = new MainWindow();
 //        JFrame frame = new WelcomeWindow();
     }
 }
