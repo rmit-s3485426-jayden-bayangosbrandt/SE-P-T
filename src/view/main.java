e3832c4515a2fe98dfdbd74c11351fb087a8db4c
Jayden Bayangos-Brandt <s3485426@student.rmit.edu.au>
2016-03-28 11:49:40 +1100

    setup to use window

--- src/view/main.java
+++ src/view/main.java
@@ -7,11 +7,10 @@ import javax.swing.*;
 
 
 public class main {
-
-    private Model model = Model.getInstance();
+    
 
     public static void main(String[] args) {
-        JFrame frame = new MainWindow();
-//        JFrame frame = new WelcomeWindow();
+        new Window();
     }
+
 }
