02e0d711cb423342d9b461812361b97772be87c8
rmit-s3466521-Alexander-Rivaldy <s3466521@student.rmit.edu.au>
2016-04-04 14:53:32 +1000

    shows area, regions and stations accordingly

---
+++ src/controller/AreaListener.java
@@ -0,0 +1,30 @@
+package controller;
+
+import model.Model;
+
+import javax.swing.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+/**
+ * Created by Alex on 31/03/2016.
+ */
+public class AreaListener implements ActionListener {
+
+    private JComboBox comboBox;
+    private Model model = Model.getInstance();
+
+    public AreaListener(JComboBox comboBox){
+    this.comboBox = comboBox;
+    }
+
+    @Override
+    public void actionPerformed(ActionEvent e) {
+    String value = (String) comboBox.getSelectedItem();
+        System.out.println(value);
+        model.changeRegion(value);
+        model.setRegionEnabled(true);
+//    model.searchRegionWeatherStation(value);
+//    System.out.println(value);
+    }
+}
