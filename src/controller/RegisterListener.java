ac729e9525b216e1b501b117e1c41a18703e5a38
Yung Garol <s3484069@student.rmit.edu.au>
2016-04-09 02:45:12 +1000

    Register listener for Welcome Page

---
+++ src/controller/RegisterListener.java
@@ -0,0 +1,28 @@
+package controller;
+
+import model.Model;
+import model.WeatherStation;
+
+import javax.swing.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+/**
+ * Created by YungYung on 9/04/2016.
+ */
+public class RegisterListener implements ActionListener {
+
+    private JTextField field;
+    private Model model = Model.getInstance();
+
+    public RegisterListener(JTextField textField){ this.field = textField;};
+
+    @Override
+    public void actionPerformed(ActionEvent e) {
+        JOptionPane.showMessageDialog(field, "Registered!");
+        String user = field.getText();
+        model.addUser(user);
+        model.setCurrentUser(user);
+        model.closeWelcome();
+    }
+}
