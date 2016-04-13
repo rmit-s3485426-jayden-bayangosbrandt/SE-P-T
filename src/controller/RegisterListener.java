30f6cb01266eff55ebdf8343c6121d2c3000e04b
Yung Garol <s3484069@student.rmit.edu.au>
2016-04-13 13:54:32 +1000

    Register won't work if username is taken or "improper"

--- src/controller/RegisterListener.java
+++ src/controller/RegisterListener.java
@@ -19,11 +19,32 @@ public class RegisterListener implements ActionListener {
 
     @Override
     public void actionPerformed(ActionEvent e) {
-        JOptionPane.showMessageDialog(field, "Registered!");
+
         String user = field.getText();
-        model.addUser(user);
-        model.setCurrentUser(user);
-        model.setEnter(true);
-        model.closeWelcome();
+        boolean successRegister = true;
+
+        // Check username content
+        if(user == "Type your name here" || user.length() > 10){
+            JOptionPane.showMessageDialog(field, "Please Type a proper username");
+            successRegister = false;
+        }
+
+        // Check if Registering New user name is already taken
+        for(int i = 0; i< model.getUserList().size(); i++) {
+            if(user.contentEquals(model.getUserList().get(i).getUsername())){
+                JOptionPane.showMessageDialog(field, "Sorry! Existing User! Please choose another name.");
+                successRegister = false;
+                break;
+            }
+        }
+
+        // Continue register success
+        if(successRegister) {
+            JOptionPane.showMessageDialog(field, "Registered!");
+            model.addUser(user);
+            model.setCurrentUser(user);
+            model.setEnter(true);
+            model.closeWelcome();
+        }
     }
 }
